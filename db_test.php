b4a898b02c2dd0cbe795bc94ea9facb072ac35fd
Connor de la Cruz <connor.c.delacruz@gmail.com>
2017-01-11 15:03:29 -0600

    removed test file

--- db_test.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'ConnectionHandler.class.php';
-
-$conn = new ConnectionHandler();
-
-$table = $conn->validateTable('test_table');
-echo "<pre>" . print_r($conn->getColumns($table),true) . "</pre>";
